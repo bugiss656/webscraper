de9291ed70b2525e2386a5e4644f164d2ca5141c
Marcin <mbugiel656@gmail.com>
2019-01-06 15:21:01 +0100

    Refactored view functions in views.py, added database models

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class PopularCurrencyModel(models.Model):
+    name = models.CharField(max_length=20)
+    rate = models.CharField(max_length=10)
+    change = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+class AllCurrencyModel(models.Model):
+    name = models.CharField(max_length=20)
+    symbol = models.CharField(max_length=10)
+    rate = models.CharField(max_length=10)
+    change = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
--- scraper/views.py
+++ scraper/views.py
@@ -1,38 +1,94 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from scraper.classModule import *
+import requests
+from bs4 import BeautifulSoup
+
+from scraper.models import PopularCurrencyModel, AllCurrencyModel
 
 def indexView(request):
     return render(request, 'homepage/homepage.html')
 
 def popularCurrencyView(request):
 
-    popularCurrencyObject = popularCurrency()
-    popularCurrencyObject.clearData()
-    popularCurrencyObject.scrapPopularCurrency()
+    url = 'https://kursy-walut.mybank.pl/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    content = soup.find('div', class_= 'cen')
+    box = content.find_all('b')
+
+    delete_query = PopularCurrencyModel.objects.all().delete()
+
+    namePattern = box[0::3]
+    ratePattern = box[1::3]
+    changePattern = box[2::3]
+
+    popularCurrencyNames = [i.text for i in namePattern]
+    popularCurrencyRates = [i.text for i in ratePattern]
+    popularCurrencyChanges = [i.text for i in changePattern]
+
+    popularCurrencyData = [
+        PopularCurrencyModel(
+            name=popularCurrencyNames[i],
+            rate=popularCurrencyRates[i],
+            change=popularCurrencyChanges[i]
+        )
+        for i in range(len(namePattern))
+    ]
+
+    add_query = PopularCurrencyModel.objects.bulk_create(popularCurrencyData)
 
     context = {
-        'currencyNames': popularCurrencyObject.popularCurrencyNames,
-        'currencyRates': popularCurrencyObject.popularCurrencyRates,
-        'currencyChanges': popularCurrencyObject.popularCurrencyChanges,
-        'dataObject': popularCurrencyObject.popularCurrencyData
+        'names': popularCurrencyNames,
+        'rates': popularCurrencyRates,
+        'changes': popularCurrencyChanges,
+        'data': popularCurrencyData
     }
 
     return render(request, 'popularCurrency/popular-currency.html', context)
 
 def allCurrencyView(request):
 
-    allCurrencyObject = allCurrency()
-    allCurrencyObject.clearData()
-    allCurrencyObject.scrapAllCurrency()
+    url = 'https://kursy-walut.mybank.pl/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+
+    table = soup.find('table', class_='tab_kursy')
+    row = table.find_all('td')
+
+    delete_query = AllCurrencyModel.objects.all().delete()
+
+    namePattern = row[0::5]
+    symbolPattern = row[1::5]
+    ratePattern = row[2::5]
+    changePattern = row[3::5]
+
+    allCurrencyNames = [i.text for i in namePattern]
+    allCurrencySymbols = [i.text for i in symbolPattern]
+    allCurrencyRates = [i.text for i in ratePattern]
+    allCurrencyChanges = [i.text for i in changePattern]
+
+    allCurrencyData = [
+        AllCurrencyModel(
+            name=allCurrencyNames[i],
+            symbol=allCurrencySymbols[i],
+            rate=allCurrencyRates[i],
+            change=allCurrencyChanges[i]
+        )
+        for i in range(len(namePattern))
+    ]
+
+    add_query = AllCurrencyModel.objects.bulk_create(allCurrencyData)
 
     context = {
-        'currencyNames': allCurrencyObject.allCurrencyNames,
-        'currencySymbols': allCurrencyObject.allCurrencySymbols,
-        'currencyRates': allCurrencyObject.allCurrencyRates,
-        'currencyChanges': allCurrencyObject.allCurrencyChanges,
-        'dataObject': allCurrencyObject.allCurrencyData
+        'names': allCurrencyNames,
+        'symbols': allCurrencySymbols,
+        'rates': allCurrencyRates,
+        'changes': allCurrencyChanges,
+        'data': allCurrencyData
     }
+
     return render(request, 'allCurrency/all-currency.html', context) 
 
 def aboutView(request):
